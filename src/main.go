13924a8d5a9cc9e81fe912bfbd378888d31c24c9
Simon <s.labzoff@yandex.ru>
2023-02-17 00:41:08 +0300

    try connect to google doc

--- src/main.go
+++ src/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	docs "google.golang.org/api/docs/v1"
 )
 
 const ResponseCodesUrl = "https://confluence.hflabs.ru/pages/viewpage.action?pageId=1181220999"
+const DocumentId = "1sY0nvgfcSFW7Y8j58gDhXNkLRtRHxQ_DgNQKwX8Lgqw"
 
 func retrieveResponseCodesHtmlPage() (string, error) {
 	resp, httpErr := http.Get(ResponseCodesUrl)
@@ -22,11 +26,37 @@ func retrieveResponseCodesHtmlPage() (string, error) {
 	return string(body), nil
 }
 
-func main() {
+func parseHtmlProcedure() {
 	respCodesHtml, err := retrieveResponseCodesHtmlPage()
 	if err != nil {
-		fmt.Errorf(err.Error())
+		fmt.Println(fmt.Errorf(err.Error()))
 	}
 
 	fmt.Println(respCodesHtml)
+}
+
+func main() {
+	ctx := context.Background()
+	fmt.Printf("ctx %v\n", ctx)
+	docsService, err := docs.NewService(ctx)
+	if err != nil {
+		fmt.Println(fmt.Errorf(err.Error()))
+		return
+	}
+
+	fmt.Printf("docsService %v\n", docsService)
+	insertTableRequest := docs.InsertTableRequest{Columns: 5, EndOfSegmentLocation: &docs.EndOfSegmentLocation{}, Location: &docs.Location{}}
+
+	fmt.Printf("insertTableRequest %v\n", insertTableRequest)
+	requests := []*docs.Request{{InsertTable: &insertTableRequest}}
+
+	batchUpdate := docs.BatchUpdateDocumentRequest{Requests: requests, WriteControl: &docs.WriteControl{}}
+	fmt.Printf("batchUpdate %v\n", batchUpdate)
+	response, err := docsService.Documents.BatchUpdate(DocumentId, &batchUpdate).Context(ctx).Do()
+	if err != nil {
+		fmt.Println(fmt.Errorf(err.Error()))
+		return
+	}
+
+	fmt.Printf("response %v\n", response)
 }
