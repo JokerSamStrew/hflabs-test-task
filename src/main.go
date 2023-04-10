bfc14d2e0c74947249e65ddc24af70687c11c6cb
Simon <s.labzoff@yandex.ru>
2023-04-10 12:16:40 +0300

    print response table

--- src/main.go
+++ src/main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 )
 
 const docId = "1sY0nvgfcSFW7Y8j58gDhXNkLRtRHxQ_DgNQKwX8Lgqw"
 
 func main() {
-	doc, err := getdocaccess(docId, "client_secret.json")
+	// codes, err := getCurrentTable()
+	// if err != nil {
+	// 	log.Fatalf("Unable to retrieve response codes info %v", err)
+	// }
+
+	// fmt.Println(codes)
+
+	// doc, err := getdocaccess(docId, "client_secret.json")
+	// if err != nil {
+	// 	log.Fatalf("Unable gain access to document: %v", err)
+	// }
+
+	// fmt.Println(doc.Title)
+
+	b, err := os.ReadFile("errorcodes.html") // just pass the file name
 	if err != nil {
-		log.Fatalf("Unable gain access to document: %v", err)
+		panic(err)
 	}
 
-	fmt.Println(doc.Title)
-
-	codes, err := getCurrentTable()
-	if err != nil {
-		log.Fatalf("Unable to retrieve response codes info %v", err)
-	}
-
-	fmt.Println(codes)
+	fmt.Println(parseResponseTable(string(b)))
 }
--- src/responsecodes.go
+++ src/responsecodes.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 const ResponseTableUrl = "https://confluence.hflabs.ru/rest/api/content/1181220999?expand=body.storage"
@@ -29,4 +33,35 @@ func getCurrentTable() (string, error) {
 
 	value := restData["body"].(map[string]interface{})["storage"].(map[string]interface{})["value"].(string)
 	return value, err
+}
+
+func parseResponseTable(responseTable string) string {
+	tkn := html.NewTokenizer(strings.NewReader(responseTable))
+
+	for {
+		tt := tkn.Next()
+
+		switch {
+
+		case tt == html.ErrorToken:
+			return "end"
+		case tt == html.StartTagToken:
+			t := tkn.Token()
+			switch {
+			case t.Data == "tr":
+				fmt.Println()
+			case t.Data == "td" || t.Data == "th":
+				fmt.Printf("\t")
+			}
+
+		case tt == html.TextToken:
+			t := tkn.Token()
+			text := strings.TrimSpace(t.Data)
+			if text == "" {
+				continue
+			}
+
+			fmt.Printf("%v", text)
+		}
+	}
 }
